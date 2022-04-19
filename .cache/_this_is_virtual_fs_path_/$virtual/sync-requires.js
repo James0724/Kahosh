38636268f123107ee62b42026078cf0815a08c1b
James Kahoro <jameskahoro07@gamail.com>
2022-04-19 10:15:29 +0300

    removed env files

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Gm Kahosh\\Desktop\\Kahosh\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\Gm Kahosh\\Desktop\\Kahosh\\src\\pages\\404.js")),
+  "component---src-pages-contentful-blog-post-title-js": preferDefault(require("C:\\Users\\Gm Kahosh\\Desktop\\Kahosh\\src\\pages\\{ContentfulBlogPost.title}.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Gm Kahosh\\Desktop\\Kahosh\\src\\pages\\index.js"))
+}
+
